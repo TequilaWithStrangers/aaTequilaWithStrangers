5212c72df37204cab460853eac2630fc57a89aa8
Erin Shields <elshields92@gmail.com>
2020-09-08 18:28:24 -0300

    Remove Events.js

--- TWS_Back/routes/Events.js
+++
@@ -1,8 +0,0 @@
-
-const express = require('express');
-const { check, validationResult } = require('express-validator');
-
-const db = require('../models');
-const { csrfProtection, asyncHandler } = require('./utils');
-
-const router = express.Router();
