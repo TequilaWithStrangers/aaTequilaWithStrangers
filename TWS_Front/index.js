8d1f16384a3225edb2b6f64e025cfd598c8c1b28
Ben Pu <benpu@Bens-iMac.local>
2020-09-09 11:50:24 -0400

    merging front end new event form

--- TWS_Front/index.js
+++ TWS_Front/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-
+const fetch = require('node-fetch');
 // Create the Express app.
 const app = express();
 
@@ -24,6 +24,12 @@ app.get("/create", (req, res) => {
 app.get("/log-in", (req, res) => {
   res.render("log-in");
 });
+
+app.get('/events/new', async(req, res) => {
+  const response = await fetch('http://localhost:8080/cities');
+  const cities = response.json();
+  res.render('new-event-form', { cities });
+})
 
 // Define a port and start listening for connections.
 const port = 4000;
