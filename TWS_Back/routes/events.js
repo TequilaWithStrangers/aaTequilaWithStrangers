c6948ffcb072307dfcc309f6b69252594b704eff
Jaron Degen <jaron.degen90@gmail.com>
2020-09-09 19:30:08 +0200

    change to events.js

---
+++ TWS_Back/routes/events.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const { check, validationResult } = require('express-validator');
+const { csrfProtection, asyncHandler } = require('./utils');
+const eventsRouter = express.Router();
+const { Event, User, City } = require('../models')
+
+eventsRouter.get('/:id(\\d+)', asyncHandler(async(req, res, next) => {
+    const id = parseInt(req.params.id); 
+    const event = await Event.findOne({
+        where: { id },
+        include: { model: City },
+        });
+
+    const host = await User.findByPk(event.hostId)
+    console.log(host)
+    // const host = await User.findByPk(hostId)
+    res.json({event, host}) 
+}));
+
+
+
+
+module.exports = eventsRouter;
