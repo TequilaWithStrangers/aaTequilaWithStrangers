321342147ed03b05b5a58fbfdeb2110baab7638e
Erin Shields <elshields92@gmail.com>
2020-09-10 17:59:31 -0300

    Add token functionality for log in and sign up

--- TWS/models/user.js
+++ TWS/models/user.js
@@ -1,11 +1,29 @@
 'use strict';
+const bcrypt = require("bcryptjs");
+
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    hashedPassword: DataTypes.STRING,
-    cityId: DataTypes.INTEGER
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    hashedPassword: {
+      type: DataTypes.STRING.BINARY,
+      allowNull: false,
+    },
+    cityId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
   }, {});
   User.associate = function (models) {
     const ops = {
@@ -14,11 +32,14 @@ module.exports = (sequelize, DataTypes) => {
       through: "Attendee"
     }
     User.belongsToMany(models.Event, ops);
-    User.hasMany(models.Attendee, {foreignKey:'userId'});
+    User.hasMany(models.Attendee, { foreignKey: 'userId' });
     User.belongsTo(models.City, { foreignKey: "cityId" })
     User.hasMany(models.Event, { foreignKey: "hostId" })
-
-
   };
+
+  User.prototype.validatePassword = function (password) {
+    return bcrypt.compareSync(password, this.hashedPassword.toString());
+  };
+
   return User;
 };
