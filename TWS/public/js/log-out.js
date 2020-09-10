79f002e141dd10a41091d7ffb4ea868c7fe8af8b
Erin Shields <elshields92@gmail.com>
2020-09-10 18:04:00 -0300

    Add log out JS

--- TWS/public/js/log-out.js
+++ TWS/public/js/log-out.js
@@ -1,33 +1,13 @@
-const logInForm = document.querySelector(".log-in-form");
+const logOutButton = document.querySelector(".log-out");
 
-logInForm.addEventListener("submit", async (e) => {
+logOutButton.addEventListener("submit", async (e) => {
     e.preventDefault();
-
-    const formData = new FormData(logInForm);
-    const email = formData.get("email");
-    const password = formData.get("password");
-
-    const body = { email, password };
     try {
-        const res = await fetch("http://localhost:8080/users/token", {
-            method: "POST",
-            body: JSON.stringify(body),
-            headers: {
-                "Content-Type": "application/json",
-            },
-        });
-        if (!res.ok) {
-            throw res;
-        }
-        const {
-            token,
-            user: { id },
-        } = await res.json();
+        localStorage.removeItem("TEQ_ACCESS_TOKEN");
+        localStorage.removeItem("TEQ_CURRENT_USER_ID");
 
-        localStorage.setItem("TEQ_ACCESS_TOKEN", token);
-        localStorage.setItem("TEQ_CURRENT_USER_ID", id);
+        window.location.href = "/log-in";
 
-        // window.location.href = "/";
     } catch (err) {
         if (err.status >= 400 && err.status < 600) {
             const errorJSON = await err.json();
